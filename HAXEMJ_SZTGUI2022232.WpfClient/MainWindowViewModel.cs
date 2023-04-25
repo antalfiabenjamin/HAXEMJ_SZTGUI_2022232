2dc5f151ddc41e5ddb59efeed8d077993e2517ac
Benjámin Antalfia <benjaminantalfia@gmail.com>
2023-04-25 19:35:56 +0200

    Created crud methods for one table

---
+++ HAXEMJ_SZTGUI2022232.WpfClient/MainWindowViewModel.cs
@@ -0,0 +1,74 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using HAXEMJ_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace HAXEMJ_SZTGUI2022232.WpfClient
+{
+    public class MainWindowViewModel : ObservableRecipient
+    {
+        public RestCollection<Phone> Phones { get; set; }
+        private Phone selectedPhone;
+
+        public Phone SelectedPhone
+        {
+            get { return selectedPhone; }
+            set 
+            {
+                if (value != null)
+                {
+                    selectedPhone = new Phone()
+                    {
+                        Name = value.Name,
+                        PhoneId = value.PhoneId,
+                    };
+                    OnPropertyChanged();
+                    (DeletePhoneCommand as RelayCommand).NotifyCanExecuteChanged();
+                }
+            }
+        }
+
+        public ICommand CreatePhoneCommand { get; set; }
+        public ICommand DeletePhoneCommand { get; set; }
+        public ICommand UpdatePhoneCommand { get; set; }
+
+        public static bool IsInDesignMode
+        {
+            get
+            {
+                var prop = DesignerProperties.IsInDesignModeProperty;
+                return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
+            }
+        }
+        public MainWindowViewModel()
+        {
+            if (!IsInDesignMode)
+            {
+                Phones = new RestCollection<Phone>("http://localhost:29971/", "phone");
+                CreatePhoneCommand = new RelayCommand(
+                    () => Phones.Add(new Phone() { Name = SelectedPhone?.Name})
+                );
+
+                DeletePhoneCommand = new RelayCommand(
+                    () =>
+                    {
+                        Phones.Delete(SelectedPhone.PhoneId);
+                    },
+                    () => SelectedPhone != null
+                );
+
+                UpdatePhoneCommand = new RelayCommand(
+                    () => Phones.Update(SelectedPhone)
+                );
+                SelectedPhone = new Phone();
+            }
+        }
+    }
+}
