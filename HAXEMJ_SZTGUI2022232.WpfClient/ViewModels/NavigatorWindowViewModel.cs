bd340c38bd2b0c65cc06440d8b5cdb6244902ccf
Benjámin Antalfia <benjaminantalfia@gmail.com>
2023-04-30 19:02:22 +0200

    Created a navigator window

---
+++ HAXEMJ_SZTGUI2022232.WpfClient/ViewModels/NavigatorWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HAXEMJ_SZTGUI2022232.WpfClient.ViewModels
+{
+    public class NavigatorWindowViewModel
+    {
+        
+    }
+}
